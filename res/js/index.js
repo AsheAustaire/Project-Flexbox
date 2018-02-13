6e09bedb389d155af5860623554ef48b70666ad4
Ashe Austaire <AsheAustaire@gmail.com>
2018-02-13 16:34:36 -0500

    refactor.active.flex-container-buttons.08

--- res/js/index.js
+++ res/js/index.js
@@ -26,11 +26,25 @@ const orderSubmit = document.body.querySelector('#order-submit')
 
 // I have changed the way I want to organize my selected items.
 
-const justifyContent = {
-  toggle: document.body.querySelector('#button-justify-content')
+const justifyContentObj = {
+  'toggle': document.body.querySelector('#button-justify-content'),
+  'textDisplay': document.body.querySelector('#text-justify-content'),
+  'counter': 1
 }
 
-const alignItems = {
+const alignItemsObj = {
+  'toggle': document.body.querySelector('#button-align-items'),
+  'textDisplay': document.body.querySelector('#text-align-items'),
+  'counter': 1
+}
+
+const alignContentObj = {
+  'toggle': document.body.querySelector('#button-align-content'),
+  'textDisplay': document.body.querySelector('#text-align-content'),
+  'counter': 1
+}
+
+const alignItemObj = {
   'start' : document.body.querySelector('#align-self-start'),
   'end' : document.body.querySelector('#align-self-end'),
   'center' : document.body.querySelector('#align-self-center'),
@@ -38,30 +52,20 @@ const alignItems = {
   'stretch' : document.body.querySelector('#align-self-stretch'),
 }
 
-const flexGrow = {
+const flexGrowObj= {
   'submit' : document.body.querySelector('#grow-submit'),
   'input' : document.body.querySelector('#grow-input')
 }
 
-const flexShrink = {
+const flexShrinkObj = {
   'submit' : document.body.querySelector('#shrink-submit'),
   'input' : document.body.querySelector('#shrink-input')
 }
 
-const flexBasis = {
+const flexBasisObj = {
   'submit' : document.body.querySelector('#basis-submit'),
   'input' : document.body.querySelector('#basis-input')
 }
-
-const flexText = {
-  'display': document.body.querySelector('#text-display'),
-  'wrap': document.body.querySelector('#text-wrap'),
-  'direction': document.body.querySelector('#text-flex-direction'),
-  'justifyContent': document.body.querySelector('#text-justify-content'),
-  'alignItems': document.body.querySelector('#text-align-items'),
-  'alignContent': document.body.querySelector('#text-align-content'),
-}
-
 
 // CONTENT EVENT LISTENERS //
 
@@ -139,10 +143,10 @@ flexbox.addEventListener('click', (e) => {
 toggleButton.addEventListener('click', () => {
   if(flexbox.style.display === 'block') {
     flexbox.style.display = 'flex'
-    flexText.display.innerText = 'display : flex'
+    flexTextObj.display.innerText = 'display : flex'
   } else {
     flexbox.style.display = 'block';
-    flexText.display.innerText = 'display : block'
+    flexTextObj.display.innerText = 'display : block'
   }
 
 
@@ -151,30 +155,30 @@ toggleButton.addEventListener('click', () => {
 toggleDirection.addEventListener('click', () => {
   if(flexbox.style.flexDirection === 'column') {
     flexbox.style.flexDirection = 'row'
-    flexText.direction.innerText = 'flex-direction : row'
+    flexTextObj.direction.innerText = 'flex-direction : row'
   } else {
     flexbox.style.flexDirection = 'column'
-    flexText.direction.innerText = 'flex-direction : column'
+    flexTextObj.direction.innerText = 'flex-direction : column'
   }
 })
 
 toggleReverse.addEventListener('click', () => {
   if(flexbox.style.flexDirection === 'row' || flexbox.style.flexDirection === 'row-reverse' || flexbox.style.flexDirection === '') {
     flexbox.style.flexDirection === 'row-reverse' ? flexbox.style.flexDirection = 'row' : flexbox.style.flexDirection = 'row-reverse';
-    flexText.direction.innerText === 'flex-direction : row' ? flexText.direction.innerText = 'flex-direction : row-reverse' : flexText.direction.innerText = 'flex-direction : row'
+    flexTextObj.direction.innerText === 'flex-direction : row' ? flexTextObj.direction.innerText = 'flex-direction : row-reverse' : flexTextObj.direction.innerText = 'flex-direction : row'
   } else {
     flexbox.style.flexDirection === 'column' ? flexbox.style.flexDirection = 'column-reverse' : flexbox.style.flexDirection = 'column';
-    flexText.direction.innerText === 'flex-direction : column' ? flexText.direction.innerText = 'flex-direction : column-reverse' : flexText.direction.innerText = 'flex-direction : column'
+    flexTextObj.direction.innerText === 'flex-direction : column' ? flexTextObj.direction.innerText = 'flex-direction : column-reverse' : flexTextObj.direction.innerText = 'flex-direction : column'
   }
 })
 
 toggleWrap.addEventListener('click', () => {
   if(flexbox.style.flexWrap === 'wrap') {
     flexbox.style.flexWrap = 'nowrap'
-    flexText.wrap.innerText = 'flex-wrap : nowrap'
+    flexTextObj.wrap.innerText = 'flex-wrap : nowrap'
   } else {
     flexbox.style.flexWrap = 'wrap'
-    flexText.wrap.innerText = 'flex-wrap : wrap'
+    flexTextObj.wrap.innerText = 'flex-wrap : wrap'
   }
 })
 
@@ -191,98 +195,96 @@ toggleWrapReverse.addEventListener('click', () => {
   }
 })
 
-// JUSTIFY CONTENT MENU
+// JUSTIFY CONTENT
 
-justifyContent.toggle.addEventListener('click', () => {
+justifyContentObj.toggle.addEventListener('click', () => {
   justifyContentCycle()
 })
 
-  let jCounter = 0
-
 function justifyContentCycle() {
-  switch(jCounter) {
+  switch(justifyContentObj.counter) {
     case 0:
       flexbox.style.justifyContent = 'flex-start'
-      flexText.justifyContent.innerText = 'justify-content : flex-start'
-      console.log(jCounter);
-      jCounter += 1
-      console.log(jCounter);
+      justifyContentObj.counter += 1
       break;
     case 1:
       flexbox.style.justifyContent = 'flex-end'
-      flexText.justifyContent.innerText = 'justify-content : flex-end'
-      console.log(jCounter);
-      jCounter += 1
+      justifyContentObj.counter += 1
       break;
     case 2:
       flexbox.style.justifyContent = 'center'
-      flexText.justifyContent.innerText = 'justify-content : center'
-      console.log(jCounter);
-      jCounter += 1
+      justifyContentObj.counter += 1
       break;
     case 3:
       flexbox.style.justifyContent = 'space-between'
-      flexText.justifyContent.innerText = 'justify-content : space-between'
-      console.log(jCounter);
-      jCounter += 1
+      justifyContentObj.counter += 1
       break;
     case 4:
       flexbox.style.justifyContent = 'space-around'
-      flexText.justifyContent.innerText = 'justify-content : space-around'
-      console.log(jCounter);
-      jCounter += 1
+      justifyContentObj.counter += 1
       break;
   }
-  (jCounter === 5) ? jCounter = 0 : false
+  justifyContentObj.textDisplay.innerText = `justify-content : ${flexbox.style.justifyContent}`;
+  (justifyContentObj.counter === 5) ? justifyContentObj.counter = 0 : false;
 }
-// ALIGN ITEMS MENU
 
-toggleAlignStart.addEventListener('click', () => {
-  flexbox.style.alignItems = 'flex-start'
-  flexText.alignItems.innerText = 'align-items : flex-start'
-})
-toggleAlignEnd.addEventListener('click', () => {
-  flexbox.style.alignItems = 'flex-end'
-  flexText.alignItems.innerText = 'align-items : flex-end'
-})
-toggleAlignCenter.addEventListener('click', () => {
-  flexbox.style.alignItems = 'center'
-  flexText.alignItems.innerText = 'align-items : center'
-})
-toggleAlignBaseline.addEventListener('click', () => {
-  flexbox.style.alignItems = 'baseline'
-  flexText.alignItems.innerText = 'align-items : baseline'
-})
-toggleAlignStretch.addEventListener('click', () => {
-  flexbox.style.alignItems = 'stretch'
-  flexText.alignItems.innerText = 'align-items : stretch'
+// ALIGN ITEMS
+
+alignItemsObj.toggle.addEventListener('click', () => {
+  alignItemsCycle()
 })
 
+function alignItemsCycle() {
+  switch(alignItemsObj.counter) {
+    case 0:
+    flexbox.style.alignItems = 'flex-start'
+    alignItemsObj.counter += 1
+      break;
+    case 1:
+    flexbox.style.alignItems = 'flex-end'
+    alignItemsObj.counter += 1
+      break;
+    case 2:
+    flexbox.style.alignItems = 'center'
+    alignItemsObj.counter += 1
+      break;
+    case 3:
+    flexbox.style.alignItems = 'baseline'
+    alignItemsObj.counter += 1
+      break;
+    case 4:
+    flexbox.style.alignItems = 'stretch'
+    alignItemsObj.counter += 1
+      break;
+  }
+  alignItemsObj.textDisplay.innerText = `align-items : ${flexbox.style.alignItems}`;
+  (alignItemsObj.counter === 5) ? alignItemsObj.counter = 0 : false;
+}
 // ALIGN CONTENT MENU
 
 toggleAlignContStart.addEventListener('click', () => {
   flexbox.style.alignContent = 'flex-start'
-  flexText.alignContent.innerText = 'align-content : flex-start'
+  flexTextObj.alignContent.innerText = 'align-content : flex-start'
 })
 toggleAlignContEnd.addEventListener('click', () => {
   flexbox.style.alignContent = 'flex-end'
-  flexText.alignContent.innerText = 'align-content : flex-end'
+  flexTextObj.alignContent.innerText = 'align-content : flex-end'
 })
 toggleAlignContCenter.addEventListener('click', () => {
   flexbox.style.alignContent = 'center'
-  flexText.alignContent.innerText = 'align-content : center'
+  flexTextObj.alignContent.innerText = 'align-content : center'
 })
 toggleAlignContStretch.addEventListener('click', () => {
   flexbox.style.alignContent = 'stretch'
-  flexText.alignContent.innerText = 'align-content : stretch'
+  flexTextObj.alignContent.innerText = 'align-content : stretch'
 })
 toggleAlignContBetween.addEventListener('click', () => {
   flexbox.style.alignContent = 'space-between'
-  flexText.alignContent.innerText = 'align-content : space-between'
+  flexTextObj.alignContent.innerText = 'align-content : space-between'
 })
 toggleAlignContAround.addEventListener('click', () => {
   flexbox.style.alignContent = 'space-around'
-  flexText.alignContent.innerText = 'align-content : space-around'
+  flexTextObj.alignContent.innerText = 'align-content : space-around'
 })
 
 // ORDER MENU
@@ -294,33 +296,33 @@ orderSubmit.addEventListener('click', (e) => {
 
 // ALIGN SELF MENU
 
-alignItems.start.addEventListener('click', () => {
+alignItemObj.start.addEventListener('click', () => {
   currentTarget.style.alignSelf = 'flex-start';
 })
-alignItems.end.addEventListener('click', () => {
+alignItemObj.end.addEventListener('click', () => {
   currentTarget.style.alignSelf = 'flex-end';
 })
-alignItems.center.addEventListener('click', () => {
+alignItemObj.center.addEventListener('click', () => {
   currentTarget.style.alignSelf = 'center';
 })
-alignItems.baseline.addEventListener('click', () => {
+alignItemObj.baseline.addEventListener('click', () => {
   currentTarget.style.alignSelf = 'baseline';
 })
-alignItems.stretch.addEventListener('click', () => {
+alignItemObj.stretch.addEventListener('click', () => {
   currentTarget.style.alignSelf = 'stretch';
 })
 
 // FLEX GROW AND SHRINK AND BASIS MENU
 
-flexGrow.submit.addEventListener('click', () => {
-  currentTarget.style.flexGrow = flexGrow.input.value
+flexGrowObj.submit.addEventListener('click', () => {
+  currentTarget.style.flexGrowObj = flexGrowObj.input.value
 })
 
 
-flexShrink.submit.addEventListener('click', () => {
-  currentTarget.style.flexShrink = flexShrink.input.value
+flexShrinkObj.submit.addEventListener('click', () => {
+  currentTarget.style.flexShrinkObj = flexShrinkObj.input.value
 })
 
-flexBasis.submit.addEventListener('click', () => {
-  currentTarget.style.flexBasis = flexBasis.input.value
+flexBasisObj.submit.addEventListener('click', () => {
+  currentTarget.style.flexBasisObj = flexBasisObj.input.value
 })
